5c0ae15dfe7237907a0ee04e1b0e1bdfeaf9c4b7
CaioVilarouca <caiovilarouca@gmail.com>
2025-08-15 00:50:26 -0300

    Lendo o arquivo CSV

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,28 +1,31 @@
 package application;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Program {
 	public static void main (String[] args) {
 				
-		File file = new File("/home/caio/eclipse-workspace/megaSenaResult/bda.csv");
-		
-		Scanner scanner = null;
-		
-		try {
-			scanner = new Scanner(file);
-			while (scanner.hasNextLine()) {
-				System.out.println(scanner.nextLine());
-			} 
-		}catch (IOException e) {
-			System.out.println(e.getMessage());
-			
-		}finally {
-			if (scanner != null) {
-				scanner.close();
-			}
+		File path = new File("/home/caio/eclipse-workspace/megaSenaResult/bda.csv");
+		List <String> file = new ArrayList<>();		
+																// Abrindo o arquivo a ser lido
+		try (BufferedReader bufferedReader = new BufferedReader(new FileReader(path))){
+            	// Le a primeira linha do arquivo
+				String line = bufferedReader.readLine();
+				while (line != null) {
+					file.add(line);
+					line = bufferedReader.readLine();// Le a proxima linha
+				}
+		} catch (IOException e) {
+			System.out.println("Error " + e.getMessage());
+		} 
+		for (String string : file) {
+			System.out.println(string);
 		}
+		System.out.println("Resutado de " + file.size() / 6 + " jogos feito");
 	}
 }
